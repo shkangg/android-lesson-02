959f7d78e32fe169b2571d7b43b2344d80052c6c
shkangg <kangseonghun123@gmail.com>
2023-09-25 15:05:06 +0900

    init code

--- src/main/java/kr/easw/lesson02/controller/AWSConroller.java
+++ src/main/java/kr/easw/lesson02/controller/AWSConroller.java
@@ -3,12 +3,20 @@ package kr.easw.lesson02.controller;
 import kr.easw.lesson02.model.dto.AWSKeyDto;
 import kr.easw.lesson02.service.AWSService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/rest/aws")
@@ -43,14 +51,40 @@ public class AWSConroller {
     }
 
 
-    @PostMapping("/download")
-    private ModelAndView onDownload(@RequestParam String fileName) {
+    //    @GetMapping("/download")
+//    private ModelAndView onDownload(@RequestParam("filename") String fileName) {
+//        try {
+//            log.info("filename={}", fileName);
+//            awsController.downloadFile(fileName);
+//           throw new IllegalStateException("기능이 구현되지 않았습니다.");
+//        } catch (Throwable e) {
+//            return new ModelAndView("redirect:/server-error?errorStatus=" + e.getMessage());
+//        }
+//    }
+    @GetMapping("/download")
+    public ResponseEntity<?> onDownload(@RequestParam("filename") String fileName) {
         try {
-           // 이곳에 파일 다운로드 로직, 혹은 서비스를 통한 다운로드 호출을 구현하십시오.
-           throw new IllegalStateException("기능이 구현되지 않았습니다.");
-        } catch (Throwable e) {
-            return new ModelAndView("redirect:/server-error?errorStatus=" + e.getMessage());
+            File file = awsController.downloadFile(fileName);
+
+            if (file == null || !file.exists()) {
+                throw new IllegalArgumentException("다운로드할 파일이 존재하지 않습니다.");
+            }
+
+            // 파일을 byte 배열로 읽어옵니다.
+            byte[] fileBytes = Files.readAllBytes(file.toPath());
+
+            // 파일의 MIME 타입을 설정합니다.
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", fileName); // 파일 다운로드 헤더 설정
+
+            // ResponseEntity를 사용하여 다운로드할 파일과 헤더 정보를 반환합니다.
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(fileBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("오류");
         }
     }
-
 }
--- src/main/java/kr/easw/lesson02/service/AWSService.java
+++ src/main/java/kr/easw/lesson02/service/AWSService.java
@@ -5,15 +5,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
 import kr.easw.lesson02.model.dto.AWSKeyDto;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
+import java.io.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,7 +30,6 @@ public class AWSService {
                 s3Client.listObjects(bucket.getName())
                         .getObjectSummaries()
                         .forEach(it -> s3Client.deleteObject(bucket.getName(), it.getKey()));
-                s3Client.deleteBucket(bucket.getName());
             }
         }
         s3Client.createBucket(BUCKET_NAME);
@@ -49,5 +46,31 @@ public class AWSService {
     @SneakyThrows
     public void upload(MultipartFile file) {
         s3Client.putObject(BUCKET_NAME, file.getOriginalFilename(), new ByteArrayInputStream(file.getResource().getContentAsByteArray()), new ObjectMetadata());
+    }
+    @SneakyThrows
+    public File downloadFile(String fileName) {
+        if (!isInitialized()) {
+            throw new IllegalStateException("AWS S3 클라이언트가 초기화되지 않았습니다.");
+        }
+        // S3에서 파일 다운로드
+        S3Object s3Object = s3Client.getObject(BUCKET_NAME, fileName);
+
+        if (s3Object == null) {
+            throw new IllegalArgumentException("다운로드할 파일이 존재하지 않습니다.");
+        }
+
+        try (S3ObjectInputStream inputStream = s3Object.getObjectContent()) {
+            File file = File.createTempFile("downloaded-", "-" + fileName);
+            try (OutputStream outputStream = new FileOutputStream(file)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+            }
+            return file;
+        } catch (IOException e) {
+            throw new RuntimeException("파일 다운로드 중 오류가 발생했습니다.", e);
+        }
     }
 }
